5e9bd1e669c77b9d74cd3b97bc5088e632e98a82
Roland Exler <roland@exler-linz.at>
2025-07-05 21:20:23 +0200

    renamed some variables

--- ScopeReadWaveform.m
+++ ScopeReadWaveform.m
@@ -1,9 +1,15 @@
-function [y, t, status] = ScopeReadWaveform(dev, channel = "CHAN1")
+% function [y, t, status] = ScopeReadWaveform(visaDev, channel = "CHAN1")
+%
+% read a waveform from Keysight MSO-X 2024 oscilloscope
+% migth work with other brands
+%
+% function is provided to show usage of viQueryBinBlock
 
-tic
-viWrite(dev, [":WAV:SOUR " channel "\n"]);
+function [y, t, status] = ScopeReadWaveform(visaDev, channel = "CHAN1")
+
+viWrite(visaDev, [":WAV:SOUR " channel "\n"]);
 % preamble_block define mapping of data values to actual properties
-preAmble   = viQuery(dev, ":WAV:PRE?\n", 200);
+preAmble   = viQuery(visaDev, ":WAV:PRE?\n", 200);
 preValues  = strsplit(preAmble, ","); % split into tokens
 wavFormat  = str2num(preValues{1});   % <format 16-bit NR1>, 0=BYTE, 1=WORD, 2=ASCII
 wavType    = str2num(preValues{2});   % <type 16-bit NR1>,
@@ -16,11 +22,11 @@ yIncrement = str2num(preValues{8});   % <yincrement 32-bit floating point NR3>,
 yOrigin    = str2num(preValues{9});   % <yorigin 32-bit floating point NR3>,
 yReference = str2num(preValues{10});  % <yreference 32-bit NR1>
 
-[data, status] = viQueryBinBlock(dev, ":WAV:DATA?\n", 2*nPoints+1000);
+[data, status] = viQueryBinBlock(visaDev, ":WAV:DATA?\n", 2*nPoints+1000);
 data = typecast(data, "uint16"); % reinterpret as uint16 (WORD)
 
-%viWrite(dev, ":WAV:DATA?\n");             % query waveform data
-%y = typecast(viReadBinBlock(dev, 1100000), "uint16"); % read and convert to word
+%viWrite(visaDev, ":WAV:DATA?\n");             % query waveform data
+%y = typecast(viReadBinBlock(visaDev, 1100000), "uint16"); % read and convert to word
 y = ( double(data)-yReference)*yIncrement + yOrigin;
 t = ((0:nPoints-1)-xReference)*xIncrement + xOrigin;
 
