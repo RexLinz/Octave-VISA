9174d30ca060f09ae4f7767a5de33c590a01c861
Roland Exler <roland@exler-linz.at>
2025-07-06 16:29:53 +0200

    added new functions to compile

--- viCompile.m
+++ viCompile.m
@@ -1,11 +1,15 @@
 % recompile all VISA (viXXX) functions
 
-% TODO try using files from C:\Program Files\IVI Foundation
+% TODO
+% * Try using files from C:\Program Files\IVI Foundation
 %   VISA\Win64\Include\visa.h
 %   VISA\Win64\Include\visytype.h
 %   VISA\Win64\Lib_x64\msc\nivisa64.lib
+% * Add README.md and make public after testing
 
-% clear all assures all files could be written to
+% In some cases mkoctfile could not write output files because
+% they have not been proper closed (may happen on error conditions)
+% "clear all" usually helps in that case
 clear all
 
 % standard VISA functions packed as oct file
@@ -13,10 +17,13 @@ mkoctfile -I. -L. -lvisa -s viOpenDefaultRM.cc
 mkoctfile -I. -L. -lvisa -s viOpen.cc
 mkoctfile -I. -L. -lvisa -s viSetAttribute.cc
 mkoctfile -I. -L. -lvisa -s viGetAttribute.cc
+mkoctfile -I. -L. -lvisa -s viConfigureSerialPort.cc
 mkoctfile -I. -L. -lvisa -s viWrite.cc
+mkoctfile -I. -L. -lvisa -s viFlush.cc
 mkoctfile -I. -L. -lvisa -s viRead.cc % might be removed, use viQuery
 mkoctfile -I. -L. -lvisa -s viReadBinBlock.cc % might be removed, use viQueryBinBlock
 mkoctfile -I. -L. -lvisa -s viClose.cc
+mkoctfile -I. -L. -lvisa -s viStatusDesc.cc % get text message for status code
 
 % extension: write/read packed as single function to reduce overhead
 mkoctfile -I. -L. -lvisa -s viQuery.cc % combined viWrite / viRead
