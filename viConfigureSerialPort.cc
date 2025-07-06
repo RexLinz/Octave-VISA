30d3c2d11b577dd02fd7b587cdc5ace7c87766ec
Roland Exler <roland@exler-linz.at>
2025-07-06 16:28:08 +0200

    new function viConfigureSerialPort

---
+++ viConfigureSerialPort.cc
@@ -0,0 +1,113 @@
+// set a VISA attribute for device
+//
+// status = viConfigureSerialPort(device, baudrate, databits, parity, stopBits, flowControl)
+//
+// see VISA manual for name and value constants (numeric)
+
+// compile and link:
+// mkoctfile -I. -L. -lvisa -s viConfigureSerialPort.cc
+
+// VISA serial attributes and values
+/*
+#define VI_ATTR_ASRL_BAUD                     (0x3FFF0021UL)
+
+#define VI_ATTR_ASRL_DATA_BITS                (0x3FFF0022UL)
+
+#define VI_ATTR_ASRL_PARITY                   (0x3FFF0023UL)
+#define VI_ASRL_PAR_NONE            (0)
+#define VI_ASRL_PAR_ODD             (1)
+#define VI_ASRL_PAR_EVEN            (2)
+#define VI_ASRL_PAR_MARK            (3)
+#define VI_ASRL_PAR_SPACE           (4)
+
+#define VI_ATTR_ASRL_STOP_BITS                (0x3FFF0024UL)
+#define VI_ASRL_STOP_ONE            (10)
+#define VI_ASRL_STOP_ONE5           (15)
+#define VI_ASRL_STOP_TWO            (20)
+
+#define VI_ATTR_ASRL_FLOW_CNTRL               (0x3FFF0025UL)
+#define VI_ASRL_FLOW_NONE           (0)
+#define VI_ASRL_FLOW_XON_XOFF       (1)
+#define VI_ASRL_FLOW_RTS_CTS        (2)
+#define VI_ASRL_FLOW_DTR_DSR        (4)
+*/
+
+#include <iostream>
+#include <octave/oct.h>
+#include <visa.h>
+
+ViSession instrument = 0;
+ViAttrState attrValue = 0;
+ViStatus  status = 0;
+
+DEFUN_DLD (viSetAttribute, args, nargout,
+  "status = viSetAttribute(device, attributeName, attributeValue)")
+{
+  if (args.length()<2)
+    error("invalid number of input arguments");
+
+  if (!args(0).is_scalar_type())
+    error("expect instrument as 1st argument");
+  instrument = args(0).int_value();
+
+  // baud rate is required
+  if (!args(1).is_scalar_type())
+    error("expect baud rate as 2nd argument");
+  attrValue = args(1).int_value();
+  status = viSetAttribute(instrument, VI_ATTR_ASRL_BAUD, attrValue);
+  if (status<0)
+    return octave_value(status);
+
+  // number of data bits (optional)
+  if (args.length() < 3)
+    attrValue = 8; // 8 data bits
+  else
+  {
+    if (!args(2).is_scalar_type())
+      error("expect baud rate as 3rd argument");
+    attrValue = args(2).int_value();
+  }
+  status = viSetAttribute(instrument, VI_ATTR_ASRL_DATA_BITS, attrValue);
+  if (status<0)
+    return octave_value(status);
+
+  // parity (optional)
+  if (args.length() < 4)
+    attrValue = VI_ASRL_PAR_NONE;
+  else
+  {
+    if (!args(3).is_scalar_type())
+      error("expect parity as 4th argument");
+    attrValue = args(3).int_value();
+  }
+  status = viSetAttribute(instrument, VI_ATTR_ASRL_PARITY, attrValue);
+  if (status<0)
+    return octave_value(status);
+
+  // number of stop bits (optional)
+  if (args.length() < 5)
+    attrValue = VI_ASRL_STOP_ONE;
+  else
+  {
+    if (!args(4).is_scalar_type())
+      error("expect stop bits as 5th argument");
+    attrValue = args(4).int_value();
+  }
+  status = viSetAttribute(instrument, VI_ATTR_ASRL_STOP_BITS, attrValue);
+  if (status<0)
+    return octave_value(status);
+
+  // flow control (optional)
+  if (args.length() < 6)
+    attrValue = VI_ASRL_FLOW_NONE;
+  else
+  {
+    if (!args(5).is_scalar_type())
+      error("expect flow control as 6th argument");
+    attrValue = args(5).int_value();
+  }
+  status = viSetAttribute(instrument, VI_ATTR_ASRL_FLOW_CNTRL, attrValue);
+
+  return octave_value(status);
+}
+
