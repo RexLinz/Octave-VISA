7a08cce9e64b68d6cddfd40a3a304e369f6f1bdf
Roland Exler <roland@exler-linz.at>
2025-07-06 16:27:17 +0200

    new function viFlush

---
+++ viFlush.cc
@@ -0,0 +1,46 @@
+// flush input and/or output queue of device
+//
+// status = viFlush(device, mask)
+//
+// see VISA manual for name and value constants (numeric)
+
+// compile and link:
+// mkoctfile -I. -L. -lvisa -s viFlush.cc
+
+// constants defined in VISA.h
+// mask could combine values from below
+#define VI_READ_BUF                 (1)
+#define VI_WRITE_BUF                (2)
+#define VI_READ_BUF_DISCARD         (4)
+#define VI_WRITE_BUF_DISCARD        (8)
+#define VI_IO_IN_BUF                (16)
+#define VI_IO_OUT_BUF               (32)
+#define VI_IO_IN_BUF_DISCARD        (64)
+#define VI_IO_OUT_BUF_DISCARD       (128)
+
+#include <iostream>
+#include <octave/oct.h>
+#include <visa.h>
+
+ViSession instrument = 0;
+ViStatus  status = 0;
+
+DEFUN_DLD (viFlush, args, nargout,
+  "status = viFlush(device, mask)")
+{
+  if (args.length()!=2)
+    error("invalid number of input arguments");
+
+  if (!args(0).is_scalar_type())
+    error("expect instrument as 1st argument");
+  instrument = args(0).int_value();
+
+  if (!args(1).is_scalar_type())
+    error("expect mask as 2nd argument");
+  ViUInt16 mask = args(1).int_value();
+
+  status = viFlush(instrument, mask);
+
+  return octave_value(status);
+}
+
