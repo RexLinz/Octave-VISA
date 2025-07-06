5f7e7f1f5c1f9d3d9538483c84f96d3a29386ca7
Roland Exler <roland@exler-linz.at>
2025-07-06 20:06:14 +0200

    added comments about library sources

--- viCompile.m
+++ viCompile.m
@@ -1,11 +1,18 @@
 % recompile all VISA (viXXX) functions
 
-% TODO
-% * Try using files from C:\Program Files\IVI Foundation
+% to get the compilation working copy the following files from your VISA
+% provider's folder.
+% If you have installed National Instruments products
+% you will find them in folder
+% C:\Program Files (x86)\National Instruments\Shared\CVI
+%   include\visa.h
+%   include\visatype.h
+%   ExtLib\msvc64\visa.lib
+% General files from IVI Foundation (headers holding some NI extensions)
+% C:\Program Files\IVI Foundation
 %   VISA\Win64\Include\visa.h
-%   VISA\Win64\Include\visytype.h
-%   VISA\Win64\Lib_x64\msc\nivisa64.lib
-% * Add README.md and make public after testing
+%   VISA\Win64\Include\visatype.h
+%   VISA\Win64\Lib_x64\msc\visa64.lib
 
 % In some cases mkoctfile could not write output files because
 % they have not been proper closed (may happen on error conditions)
@@ -13,22 +20,22 @@
 clear all
 
 % standard VISA functions packed as oct file
-mkoctfile -I. -L. -lvisa -s viOpenDefaultRM.cc
-mkoctfile -I. -L. -lvisa -s viOpen.cc
-mkoctfile -I. -L. -lvisa -s viSetAttribute.cc
-mkoctfile -I. -L. -lvisa -s viGetAttribute.cc
-mkoctfile -I. -L. -lvisa -s viConfigureSerialPort.cc
-mkoctfile -I. -L. -lvisa -s viWrite.cc
-mkoctfile -I. -L. -lvisa -s viFlush.cc
-mkoctfile -I. -L. -lvisa -s viRead.cc % might be removed, use viQuery
-mkoctfile -I. -L. -lvisa -s viReadBinBlock.cc % might be removed, use viQueryBinBlock
-mkoctfile -I. -L. -lvisa -s viClose.cc
-mkoctfile -I. -L. -lvisa -s viStatusDesc.cc % get text message for status code
+mkoctfile -I. -L. -lvisa64 -s viOpenDefaultRM.cc
+mkoctfile -I. -L. -lvisa64 -s viOpen.cc
+mkoctfile -I. -L. -lvisa64 -s viSetAttribute.cc
+mkoctfile -I. -L. -lvisa64 -s viGetAttribute.cc
+mkoctfile -I. -L. -lvisa64 -s viConfigureSerialPort.cc
+mkoctfile -I. -L. -lvisa64 -s viWrite.cc
+mkoctfile -I. -L. -lvisa64 -s viFlush.cc
+mkoctfile -I. -L. -lvisa64 -s viRead.cc % consider using viQuery
+mkoctfile -I. -L. -lvisa64 -s viReadBinBlock.cc % consider using viQueryBinBlock
+mkoctfile -I. -L. -lvisa64 -s viClose.cc
+mkoctfile -I. -L. -lvisa64 -s viStatusDesc.cc % get text message for status code
 
 % extension: write/read packed as single function to reduce overhead
-mkoctfile -I. -L. -lvisa -s viQuery.cc % combined viWrite / viRead
-mkoctfile -I. -L. -lvisa -s viQueryBinBlock.cc % combined viWrite viReadBinBlock
+mkoctfile -I. -L. -lvisa64 -s viQuery.cc % combined viWrite / viRead
+mkoctfile -I. -L. -lvisa64 -s viQueryBinBlock.cc % combined viWrite viReadBinBlock
 
 % NI example RdWrt.c converted to octave
-mkoctfile -I. -L. -lvisa -s VISAtest.cc
+mkoctfile -I. -L. -lvisa64 -s VISAtest.cc
 
