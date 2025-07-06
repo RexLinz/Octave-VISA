5110a610fe1bcbc2c117edb860e9a8efebb67998
Roland Exler <roland@exler-linz.at>
2025-07-06 16:27:46 +0200

    Use try/catch on test functions

--- viTest.m
+++ viTest.m
@@ -6,77 +6,88 @@
 % open resource manager
 [visaRM, status] = viOpenDefaultRM;
 if status<0
+  clear visaRM; % ensure we could no longer use
   error("open resource manager failed");
 end
+% device manager is opened
+try
+  % open connection to device, set terminator to 10 (\n)
+  [visaDev, status] = viOpen(visaRM, "COM3", 3000, 10);
+  if status<0
+    error("open device failed");
+  end
+  % ...some typical operations
 
-% open connection to device, set terminator to 10 (\n)
-[visaDev, status] = viOpen(visaRM, "COM3", 2000, 10);
-if status<0
-  error("open device failed");
-end
+  # some frequently used attribute values from visa.h
+  #define VI_ATTR_TMO_VALUE                     (0x3FFF001AUL)
 
-# some frequently used attribute values from visa.h
-#define VI_ATTR_TMO_VALUE                     (0x3FFF001AUL)
+  #define VI_ATTR_TERMCHAR                      (0x3FFF0018UL)
+  #define VI_ATTR_TERMCHAR_EN                   (0x3FFF0038UL)
 
-#define VI_ATTR_TERMCHAR                      (0x3FFF0018UL)
-#define VI_ATTR_TERMCHAR_EN                   (0x3FFF0038UL)
+  #define VI_ATTR_RD_BUF_SIZE                   (0x3FFF002BUL)
+  #define VI_ATTR_WR_BUF_SIZE                   (0x3FFF002EUL)
 
-#define VI_ATTR_RD_BUF_SIZE                   (0x3FFF002BUL)
-#define VI_ATTR_WR_BUF_SIZE                   (0x3FFF002EUL)
+  #define VI_ATTR_ASRL_BAUD                     (0x3FFF0021UL)
+  #define VI_ATTR_ASRL_DATA_BITS                (0x3FFF0022UL)
+  #define VI_ATTR_ASRL_PARITY                   (0x3FFF0023UL)
+  #define VI_ATTR_ASRL_STOP_BITS                (0x3FFF0024UL)
+  #define VI_ATTR_ASRL_FLOW_CNTRL               (0x3FFF0025UL)
 
-#define VI_ATTR_ASRL_BAUD                     (0x3FFF0021UL)
-#define VI_ATTR_ASRL_DATA_BITS                (0x3FFF0022UL)
-#define VI_ATTR_ASRL_PARITY                   (0x3FFF0023UL)
-#define VI_ATTR_ASRL_STOP_BITS                (0x3FFF0024UL)
-#define VI_ATTR_ASRL_FLOW_CNTRL               (0x3FFF0025UL)
+  % should already be done if you use the termchar option in the viOpen function
+  status = viSetAttribute(visaDev, 0x3FFF0038, 1); % VI_ATTR_TERMCHAR_EN
+  if status<0
+    error("enable termchar failed");
+  end
 
-% should already be done if you use the termchar option in the viOpen function
-status = viSetAttribute(visaDev, 0x3FFF0038, 1); % VI_ATTR_TERMCHAR_EN
-if status<0
-  warning("enable termchar failed");
-end
+  % check timeout set by viOpen
+  [timeout, status] = viGetAttribute(visaDev,0x3FFF001A); % VI_ATTR_TMO_VALUE
+  if (status<0) | (timeout~=3000)
+    error("get attribute failed");
+  end
+  % change timout
+  status = viSetAttribute(visaDev, 0x3FFF001A, 2000); % VI_ATTR_TMO_VALUE
+  if status<0
+    error("set attribute failed");
+  end
+  % check if change has been successful
+  [timeout, status] = viGetAttribute(visaDev,0x3FFF001A); % VI_ATTR_TMO_VALUE
+  if (status<0) | (timeout~=2000)
+    error("get attribute failed");
+  end
 
-% check timeout set by viOpen
-[timeout, status] = viGetAttribute(visaDev,0x3FFF001A); % VI_ATTR_TMO_VALUE
-if (status<0) | (timeout~=2000)
-  warning("get attribute failed");
-end
-% change timout
-status = viSetAttribute(visaDev, 0x3FFF001A, 5000); % VI_ATTR_TMO_VALUE
-if status<0
-  warning("set attribute failed");
-end
-% check if change has been successful
-[timeout, status] = viGetAttribute(visaDev,0x3FFF001A); % VI_ATTR_TMO_VALUE
-if (status<0) | (timeout~=5000)
-  warning("get attribute failed");
-end
+  % write *IDN? query to device
+  status = viWrite(visaDev, "*IDN?\n");
+  if status<0
+    error("write failed");
+  end
+  % read response
+  [response, status] = viRead(visaDev, 100);
+  if status<0
+    error("read device failed");
+  end
+  disp(["write '*IDN?', read '" strtrim(response) "'"]); % skip \n
 
-% write *IDN? query to device
-status = viWrite(visaDev, "*IDN?\n");
-if status<0
-  warning("write failed");
-end
-% read response
-[response, status] = viRead(visaDev, 100);
-if status<0
-  warning("read device failed");
-end
-disp(["write *IDN? / read\n  " strtrim(response)]); % skip \n
+  % easier solution instead of using viWrite/viRead (but about same performance)
+  [response, status] = viQuery(visaDev, "*STB?\n", 100);
+  if status<0
+    error("query device failed");
+  end
+  disp(["query '*STB?', read '" strtrim(response) "'"]); % skip \n
 
-% more comftable solution instead of using viWrite/viRead (about same performance)
-[response, status] = viQuery(visaDev, "*IDN?\n", 100);
-if status<0
-  error("query device failed");
-end
-disp(["query *IDN?\n  " strtrim(response)]); % skip \n
+catch
+  errorMessage = lasterr % show last error message
+  statusMessage = viStatusDesc(status) % explain error
+  if visaDev>0 % we have an open connection?
+    % not required if you close the resource manager next
+    status = viClose(visaDev); clear visaDev; % ensure we could no longer use
+    if status<0
+      error("close device failed");
+    end
+  end
+end_try_catch
 
-% not required if you close the resource manager next
-status = viClose(visaDev); clear visaDev; % ensure we could no longer use
-if status<0
-  error("close device failed");
-end
-
+clear visaDev; % ensure we could no longer use
+% close the resource manager (will also close all device connections)
 status = viClose(visaRM); clear visaRM; % ensure we could no longer use
 if status<0
   error("close resource manager failed");
--- viTestQueryBinBlock.m
+++ viTestQueryBinBlock.m
@@ -15,84 +15,89 @@
 % open resource manager
 [visaRM, status] = viOpenDefaultRM;
 if status<0
+  clear visaRM; % ensure we could no longer use
   error("open resource manager failed");
 end
-
-% open connection to device, set terminator to 10 (\n)
-[visaDev, status] = viOpen(visaRM, "MSO", 2000, 10);
-if status<0
-  error("open device failed");
-end
-
-% using viQery instead of viWrite/viRead
-[response, status] = viQuery(visaDev, "*IDN?\n", 100);
-if status<0
-  error("query device failed");
-end
-disp(["query *IDN?\n  " srtrim(response)]); % skip \n
-
-% try to read a waveform from channel 1 as array of WORD
-% number of waveform points returned will depend on the scope settings
-if 1 % enable or disable
-  % set up scope
-  viWrite(visaDev, ":WAV:FORM WORD\n");  % format WORD
-  viWrite(visaDev, ":WAV:BYT LSBF\n");   % LSB first, required for 16 bit data
-  viWrite(visaDev, ":WAV:POINTS MAX\n"); % record length
-  % stop acquisition to give time to read all channels
-  viWrite(visaDev, ":STOP\n"); % stop to get all waveforms from same ackquisition
-
-  tic
-  disp("reading channel 1 data");
-  [y1, t] = ScopeReadWaveform(visaDev, "CHAN1");
-  disp("reading channel 2 data");
-  [y2, t] = ScopeReadWaveform(visaDev, "CHAN2"); % reduntant time vector
-  toc % about 300 ms per channel for 500k points on WiFi
-
-  % restart acquisition
-  viWrite(visaDev, ":RUN\n");
-
-  % create plot
-  figure(1, "name", "Scope Waveform");
-  plot(
-    1000*t, y1, ";CH1;",
-    1000*t, y2, ";CH2;"
-  );
-  grid on;
-  xlabel("t [ms]");
-end
-
-% read a screenshot as PNG from the scope
-if 0
-  % set up scope (optional)
-  viWrite(visaDev, ":HARDcopy:INKSaver OFF\n"); % OFF = black background
-  % get image in PNG file format
-%  viWrite(visaDev, ":DISP:DATA? PNG,COL\n");
-%  [imgData, status] = viReadBinBlock(visaDev, 200000);
-  [imgData, status] = viQueryBinBlock(visaDev, ":DISP:DATA? PNG,COL\n", 200000); % return uint8
-  if status==0
-    % write to temporary file
-    fid = fopen("viTest.png", "wb");
-    fwrite(fid, imgData(1:end-1)); % skip \n
-    fclose(fid);
-    % display image from file
-    figure(2, "name", "Scope Screenshot");
-    imshow("viTest.png");
-  else
-    disp("read image failed");
+% device manager is opened
+try
+  % open connection to device, set terminator to 10 (\n)
+  [visaDev, status] = viOpen(visaRM, "MSO", 2000, 10);
+  if status<0
+    error("open device failed");
   end
-end
 
-% check if further communication is still fine after readBinBlock
-[response, status] = viQuery(visaDev, "*IDN?\n", 100);
-if status<0
-  error("regular communication after readBinBlock failes");
-end
+  % try to read waveforms as array of WORD
+  % number of waveform points returned will depend on the scope settings
+  if 1 % enable or disable
+    % channels 1 and 2 must be enabled
+    viWrite(visaDev, ":CHAN1:DISP ON\n");
+    viWrite(visaDev, ":CHAN2:DISP ON\n");
+    % set up waveform readout
+    viWrite(visaDev, ":WAV:FORM WORD\n");  % format WORD
+    viWrite(visaDev, ":WAV:BYT LSBF\n");   % LSB first, required for 16 bit data
+    viWrite(visaDev, ":WAV:POINTS MAX\n"); % record length
+    % stop acquisition to give time to read all channels
+    viWrite(visaDev, ":STOP\n"); % stop to get all waveforms from same ackquisition
 
-% not required if you close the resource manager next
-status = viClose(visaDev); clear visaDev; % ensure we could no longer use
-if status<0
-  error("close device failed");
-end
+    tic
+    disp("reading channel 1 data");
+    [y1, t] = ScopeReadWaveform(visaDev, "CHAN1");
+    disp("reading channel 2 data");
+    [y2, t] = ScopeReadWaveform(visaDev, "CHAN2"); % reduntant time vector
+    toc % about 300 ms per channel for 500k points on WiFi
+
+    % restart acquisition
+    viWrite(visaDev, ":RUN\n");
+
+    % create plot
+    figure(1, "name", "Scope Waveform");
+    plot(
+      1000*t, y1, ";CH1;",
+      1000*t, y2, ";CH2;"
+    );
+    grid on;
+    xlabel("t [ms]");
+  end
+
+  % read a screenshot as PNG from the scope
+  if 0 % enable or disable
+    % set up scope (optional)
+    viWrite(visaDev, ":HARDcopy:INKSaver OFF\n"); % OFF = black background
+    % get image in PNG file format
+  %  viWrite(visaDev, ":DISP:DATA? PNG,COL\n");
+  %  [imgData, status] = viReadBinBlock(visaDev, 200000);
+    [imgData, status] = viQueryBinBlock(visaDev, ":DISP:DATA? PNG,COL\n", 200000); % return uint8
+    if status==0
+      % write to temporary file
+      fid = fopen("viTest.png", "wb");
+      fwrite(fid, imgData(1:end-1)); % skip \n
+      fclose(fid);
+      % display image from file
+      figure(2, "name", "Scope Screenshot");
+      imshow("viTest.png");
+    else
+      disp("read image failed");
+    end
+  end
+
+  % check if further communication is still fine after read/query BinBlock
+  [response, status] = viQuery(visaDev, "*IDN?\n", 100);
+  if status<0
+    error("regular communication after readBinBlock failed");
+  end
+
+catch
+  errorMessage = lasterr % show last error message
+  statusMessage = viStatusDesc(status) % explain error
+  if visaDev>0 % we have an open connection?
+    % not required if you close the resource manager next
+    status = viClose(visaDev); clear visaDev; % ensure we could no longer use
+    if status<0
+      error("close device failed");
+    end
+  end
+  clear visaDev; % ensure we could no longer use
+end_try_catch
 
 % close resource manager
 status = viClose(visaRM); clear visaRM; % ensure we could no longer use
