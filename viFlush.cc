fea81f928368a00570a02bff14d1938ca6ae27c9
Roland Exler <roland@exler-linz.at>
2025-07-06 20:07:25 +0200

    set default mask to flush read and write buffers

--- viFlush.cc
+++ viFlush.cc
@@ -1,6 +1,6 @@
 // flush input and/or output queue of device
 //
-// status = viFlush(device, mask)
+// status = viFlush(device, mask=(VI_READ_BUF | VI_WRITE_BUF))
 //
 // see VISA manual for name and value constants (numeric)
 
@@ -9,6 +9,7 @@
 
 // constants defined in VISA.h
 // mask could combine values from below
+/*
 #define VI_READ_BUF                 (1)
 #define VI_WRITE_BUF                (2)
 #define VI_READ_BUF_DISCARD         (4)
@@ -17,6 +18,7 @@
 #define VI_IO_OUT_BUF               (32)
 #define VI_IO_IN_BUF_DISCARD        (64)
 #define VI_IO_OUT_BUF_DISCARD       (128)
+*/
 
 #include <iostream>
 #include <octave/oct.h>
@@ -26,21 +28,23 @@ ViSession instrument = 0;
 ViStatus  status = 0;
 
 DEFUN_DLD (viFlush, args, nargout,
-  "status = viFlush(device, mask)")
+  "status = viFlush(device, mask=(VI_READ_BUF | VI_WRITE_BUF))")
 {
-  if (args.length()!=2)
+  if (args.length()<1)
     error("invalid number of input arguments");
 
   if (!args(0).is_scalar_type())
     error("expect instrument as 1st argument");
   instrument = args(0).int_value();
 
-  if (!args(1).is_scalar_type())
-    error("expect mask as 2nd argument");
-  ViUInt16 mask = args(1).int_value();
-
+  ViUInt16 mask = (VI_READ_BUF | VI_WRITE_BUF);
+  if (args.length()>1)
+  {
+    if (!args(1).is_scalar_type())
+      error("expect mask as 2nd argument");
+    mask = args(1).int_value();
+  }
   status = viFlush(instrument, mask);
-
   return octave_value(status);
 }
 
