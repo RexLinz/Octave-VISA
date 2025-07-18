22b75e0fe68b5b748ce0b47d78fdcc01fc7bdbbe
Roland Exler <roland@exler-linz.at>
2025-07-18 16:12:35 +0200

    reworked and tested bode plotter

--- bodePlot.m
+++ bodePlot.m
@@ -1,58 +1,5 @@
 % Agilent 33250A / MSO-X 2024 bode plotter
 
-% 33250A *LRN? string, send *RST before to ensure proper processing
-% Return Count: 1086 bytes
-%  :FUNC:SQU:DCYC +5.0000000000000E+01;
-%  :FUNC:RAMP:SYMM +1.0000000000000E+02;
-%  :FUNC:USER SCHOTTER_20;
-%  :FUNC SIN;
-%  :FREQ +1.0000000000000E+02;
-%  :PULS:WIDT +1.0000000000000E-03;
-%  :PULS:TRAN +5.0000000000000E-09;
-%  :OUTP:LOAD 9.9E37;
-%  :VOLT:UNIT VPP;
-%  :VOLT +5.0000000000000E+00;
-%  :VOLT:OFFS +0.0000000000000E+00;
-%  :VOLT:RANG:AUTO 1;
-%  :TRIG:SOUR BUS;
-%  :TRIG:SLOP POS;
-%  :TRIG:DEL +0.0000000000000E+00;
-%  :UNIT:ANGL DEG;
-%  :BURS:MODE TRIG;
-%  :BURS:NCYC +1.0000000000000E+00;
-%  :BURS:PHAS +0.0000000000000E+00;
-%  :BURS:INT:PER +4.0000000000000E+00;
-%  :BURS:GATE:POL NORM;
-%  :BURS:STAT 0;
-%  :FREQ:STAR +4.0000000000000E+03;
-%  :FREQ:STOP +5.0000000000000E+03;
-%  :MARK 0;
-%  :MARK:FREQ +5.0000000000000E+02;
-%  :SWE:SPAC LIN;
-%  :SWE:TIME +2.0000000000000E+00;
-%  :SWE:STAT 0;
-%  :AM:DEPT +1.0000000000000E+02;
-%  :AM:SOUR INT;
-%  :AM:INT:FREQ +2.0000000000000E+04;
-%  :AM:INT:FUNC SIN;
-%  :AM:STAT 0;
-%  :FM:DEV +1.8300000000000E+04;
-%  :FM:SOUR INT;
-%  :FM:INT:FREQ +5.0000000000000E+03;
-%  :FM:INT:FUNC SIN;
-%  :FM:STAT 0;
-%  :FSK:FREQ +1.0000000000000E+02;
-%  :FSK:SOUR INT;
-%  :FSK:INT:RATE +1.0000000000000E+01;
-%  :FSK:STAT 0;
-%  :OUTP 1;
-%  :OUTP:POL NORM;
-%  :OUTP:SYNC 1;
-%  :OUTP:TRIG:SLOP POS;
-%  :OUTP:TRIG 0;
-%  :FORM:BORD NORM;
-%  :DISP 1.
-
 global visaRM
 global visaFG
 global visaMSO
@@ -76,10 +23,11 @@ uiFig = figure(1,
   "name", "bode plotter",
   "toolbar", "none",
   "menubar", "none",
-  "position", [24 206 780 500]
+  "position", [24 206 790 470]
 );
 clf;
 
+% open and initialze function generator with defaults
 function initFG(source, event)
   global visaRM
   if visaRM==0
@@ -93,25 +41,26 @@ function initFG(source, event)
   end
   if visaFG>0
 %    viWrite(visaFG, "*RST\n");
-    viWrite(visaFG, ":FUNC SIN\n");
-    viWrite(visaFG, ":FREQ +1000.0\n");
-    viWrite(visaFG, ":OUTP:LOAD 9.9E37\n");
-    viWrite(visaFG, ":VOLT:UNIT VPP\n");
-    viWrite(visaFG, ":VOLT +0.1\n");
-    viWrite(visaFG, ":VOLT:OFFS +0.0\n");
-    viWrite(visaFG, ":VOLT:RANG:AUTO 1\n");
-    viWrite(visaFG, ":BURS:STAT 0\n");
-    viWrite(visaFG, ":SWE:STAT 0\n");
-    viWrite(visaFG, ":AM:STAT 0\n");
-    viWrite(visaFG, ":FM:STAT 0\n");
-    viWrite(visaFG, ":FSK:STAT 0\n");
-    viWrite(visaFG, ":OUTP 1\n");
+    viWrite(visaFG, ":FUNC SIN\n");         % sinewave
+    viWrite(visaFG, ":FREQ +1000.0\n");     % 1kHz
+    viWrite(visaFG, ":OUTP:LOAD 9.9E37\n"); % high impedance load
+    viWrite(visaFG, ":VOLT:UNIT VPP\n");    % programming as VPP
+    viWrite(visaFG, ":VOLT +1.0\n");        % 1.0 Vpp
+    viWrite(visaFG, ":VOLT:OFFS +0.0\n");   % no offset
+    viWrite(visaFG, ":VOLT:RANG:AUTO 1\n"); % output range (attenuator)
+    viWrite(visaFG, ":BURS:STAT 0\n");      % no burst
+    viWrite(visaFG, ":SWE:STAT 0\n");       % no sweep
+    viWrite(visaFG, ":AM:STAT 0\n");        % no AM
+    viWrite(visaFG, ":FM:STAT 0\n");        % no FM
+    viWrite(visaFG, ":FSK:STAT 0\n");       % no FSK
+    viWrite(visaFG, ":OUTP 1\n");           % output on
   else
     disp("  no connection to function generator");
   end
 end
 uimenu("text", "init &Generator", "accelerator", "g", "menuselectedfcn", @initFG);
 
+% open and initialize oscilloscope with default
 function MSO = initMSO(source, event)
   global visaRM;
   if visaRM==0
@@ -120,58 +69,64 @@ function MSO = initMSO(source, event)
   global visaMSO;
   disp("initialize scope");
   if visaMSO==0
-    [visaMSO, status] = viOpen(visaRM, "MSO-USB");
+    [visaMSO, status] = viOpen(visaRM, "MSO-USB", 5000, 10);
   end
   if visaMSO>0
-    viWrite(visaMSO, ":CHAN1:DISP ON\n");
-    viWrite(visaMSO, ":CHAN1:BWLimit ON\n");
-    viWrite(visaMSO, ":CHAN1:COUPling AC\n");    % enable channels
-    viWrite(visaMSO, ":CHAN1:RANGE 8.0\n");  % full scale range (V or A)
-    viWrite(visaMSO, ":CHAN1:OFFset 0.0\n");   % V or A
-    viWrite(visaMSO, ":CHAN1:UNIT VOLT\n");
-    viWrite(visaMSO, ":CHAN1:LAB \"IN\"");    %   coupling AC
+    % get input and output channels
+    global configTable;
+    config = get(configTable, "data"); % vector of numbers in column 2
+    labels = {"IN", "OUT"}; % trace labels
+    for c = 1:2 % line indices to table
+      channelNumber = cell2mat(config(c+8,2));
+      chString = [":CHAN" num2str(channelNumber)];
+      viWrite(visaMSO, [chString ":DISP ON\n"]);     % enable channel
+      viWrite(visaMSO, [chString ":BWLimit ON\n"]);  % 20 MHz bandwidth
+      viWrite(visaMSO, [chString ":COUPling AC\n"]); % AC coupling
+      viWrite(visaMSO, [chString ":RANGE 8.0\n"]);   % full scale range
+      viWrite(visaMSO, [chString ":OFFset 0.0\n"]);  % no offset
+      viWrite(visaMSO, [chString ":UNIT VOLT\n"]);   % voltage displayed
+      viWrite(visaMSO, [chString ":LAB \"" char(labels(c)) "\""]);
+    end
+    viWrite(visaMSO, ":DISPlay:LABel ON\n");       % display labels
 
-    viWrite(visaMSO, ":CHAN2:DISP ON\n");
-    viWrite(visaMSO, ":CHAN2:BWLimit ON\n");
-    viWrite(visaMSO, ":CHAN2:COUPling AC\n");    % enable channels
-    viWrite(visaMSO, ":CHAN2:RANGE 8.0\n");  % full scale range (V or A)
-    viWrite(visaMSO, ":CHAN2:OFFset 0.0\n");   % V or A
-    viWrite(visaMSO, ":CHAN2:UNIT VOLT\n");
-    viWrite(visaMSO, ":CHAN2:LAB \"OUT\"");    %   coupling AC
+    viWrite(visaMSO, ":TRIG:MODE EDGE\n");         % trigger on edge
+    viWrite(visaMSO, ":TRIG:EDGE:SLOP POS\n");     % positive edge
+    channelNumber = cell2mat(config(9,2));         % input to DUT
+    chString = ["CHAN" num2str(channelNumber)];
+    viWrite(visaMSO, [":TRIG:EDGE:LEV 0," chString "\n"]);  % trigger on channelIn
 
-    viWrite(visaMSO, ":DISPlay:LABel ON\n");
+    % NOTE: averaging significantly slowing down on low frequency
+    switch 2
+      case 1 % averaging
+        viWrite(visaMSO, ":ACQuire:TYPE AVERage\n");
+        viWrite(visaMSO, ":ACQuire:COUNt 8\n");
+      case 2 % hires
+        viWrite(visaMSO, ":ACQuire:TYPE HRES\n");
+      otherwise % normal
+        viWrite(visaMSO, ":ACQuire:TYPE NORM\n");
+    end
 
-    viWrite(visaMSO, ":TRIG:MODE EDGE\n");
-    viWrite(visaMSO, ":TRIG:EDGE:SLOP POS\n");
-    viWrite(visaMSO, ":TRIG:EDGE:LEV 0,CHAN1\n");
-
-    viWrite(visaMSO, ":TIM:REFerence CENTer\n");
-    viWrite(visaMSO, ":TIM:RANGE 0.005\n");   % full screen
-
-    viWrite(visaMSO, ":MEASure:CLEar\n");
-    viWrite(visaMSO, ":MEASure:PHASe CHAN1,CHAN2\n");
-    viWrite(visaMSO, ":MEASure:SHOW ON\n");
-    % :MEASure:SOURce CHAN1
-    viWrite(visaMSO, ":MEASure:VPP CHAN1\n");
-%    viWrite(visaMSO, ":MEASure:VRMS CYCLe,AC,CHAN1\n");
-    viWrite(visaMSO, ":MEASure:VPP CHAN2\n");
-%    viWrite(visaMSO, ":MEASure:VRMS CYCLe,AC,CHAN2\n");
+    viWrite(visaMSO, ":TIM:REFerence CENTer\n"); % trigger point in center
+    viWrite(visaMSO, ":TIM:RANGE 0.005\n");      % 5 periods of 1 kHz
   else
     disp("  no connection to scope");
   end
 end
 uimenu("text", "init &MSO", "accelerator", "m", "menuselectedfcn", @initMSO);
 
+% load data for display
 function openFile(source, event)
   selectedFile = uigetfile("*.bin", "select data to load");
   if ~isnumeric(selectedFile)
     load(selectedFile);
     global dataTable;
     set(dataTable, "data", data);
+    updatePlot([], []);
   end
 end
 uimenu("text", "&Open file", "accelerator", "o", "menuselectedfcn", @openFile);
 
+% save data for external processing
 function saveFile(source, event)
   selectedFile = uiputfile("*.bin", "select file to save data");
   saveOK = 0;
@@ -190,6 +145,30 @@ function saveFile(source, event)
 end
 uimenu("text", "&Save file", "accelerator", "s", "menuselectedfcn", @saveFile);
 
+% create empty data table with just freqency column filled
+function data = defaultData()
+  global configTable;
+  config = get(configTable, "data"); % vector of numbers
+  fStart = cell2mat(config(2,2));
+  fStop = cell2mat(config(3,2));
+  nPoints = cell2mat(config(4,2));
+  logF = cell2mat(config(5,2));
+  data = NA(nPoints, 6);
+  if logF==1
+    f = logspace(log10(fStart), log10(fStop), nPoints);
+  else
+    f = linspace(log10(fStart), log10(fStop), nPoints);
+  end
+  data(:,1) = f;
+end
+% set dataTable to defaults after change of parameters
+function setDefaultData(source, event)
+  global dataTable;
+  data = defaultData();
+  set(dataTable, "data", data);
+end
+
+% generate configuration table
 global configTable;
 configTable = uitable(uiFig,
   "tag", "parameters",
@@ -202,27 +181,100 @@ configTable = uitable(uiFig,
   "columneditable", [false true false],
   "data",
   {
+    "GENERATOR", "", ""
     "f start", 100, "Hz"
     "f stop", 10000, "Hz"
     "n", 11, "Points"
     "log freq.?", 1, "1/0"
     "Ampl.", 1.0, "Vpp"
-    "CHAN", 1, "input"
-    "CHAN", 2, "output"
-  }
+    "", "", ""
+    "SCOPE", "", ""
+    "channel", 1, "input"
+    "channel", 2, "output"
+  },
+  "celleditcallback", @setDefaultData
 );
 
+% generate data table
 global dataTable;
 dataTable = uitable(uiFig,
   "tag", "data",
   "units", "normalized",
   "position", [0.26 0.0 0.74 1.0],
   "rowstriping", "on",
-  "columnname", {"f", "in", "out", "gain", "gain [dB]" "phase"},
+  "columnname", {"f [Hz]", "Vin [Vrms]", "Vout [Vrms]", "gain [1]", "gain [dB]" "phase [deg]"},
   "columnwidth", {90 90 90 90 90 90}
 );
 
+% update plot data displayed
+function updatePlot(source, event)
+  figure(2);
+  clf;
+  global dataTable;
+  data = get(dataTable, "data");
+  if rows(data)<1
+    return; % no data to plot
+  end
+  styleControls = get(dataTable, "userdata");
+  xStyle = get(styleControls(1), "value");
+  yStyle = get(styleControls(2), "value");
+  % magnitude
+  hGain = subplot(2,1,1);
+  switch yStyle
+    case 3 % linear
+      plot(data(:,1), data(:,4));
+      ylabel("gain [1]");
+    case 2 % log
+      semilogy(data(:,1), data(:,4));
+      ylabel("gain [1]");
+    otherwise % dB
+      plot(data(:,1), data(:,5));
+      ylabel("gain [dB]");
+  end
+  grid on;
+  xlabel("f [Hz]");
+  % phase
+  hPhase = subplot(2,1,2);
+  plot(data(:,1), rad2deg(unwrap(deg2rad(data(:,6))))); grid on;
+  xlabel("f [Hz]");
+  ylabel("phase [deg]");
+  switch xStyle
+    case 2
+    set(hGain, "XScale", "lin");
+    set(hPhase, "XScale", "lin");
+  otherwise
+    set(hGain, "XScale", "log");
+    set(hPhase, "XScale", "log");
+  end
+  linkaxes([hGain, hPhase], "x");
+end
+
+% X axis control
+global xControl;
+xControl = uicontrol(uiFig,
+  "style", "popupmenu",
+  "units", "normalized",
+  "position", [0.0 0.1 0.26 0.1],
+  "string", {"x Axis logarithmic", "x Axis linear"},
+  "value", 1,
+  "callback", @updatePlot
+);
+% Y axis control
+global yControl;
+yControl = uicontrol(uiFig,
+  "style", "popupmenu",
+  "units", "normalized",
+  "position", [0.0 0.2 0.26 0.1],
+  "string", {"y Axis dB", "y Axis logarithmic", "y Axis linear"},
+  "value", 1,
+  "callback", @updatePlot
+);
+% make available for updatePlot() function
+set(dataTable, "userdata", [xControl, yControl]);
+
 % helping functions
+
+% change signal frequency
 function FGsetFrequency(f)
   global visaFG;
   if visaFG>0
@@ -232,6 +284,7 @@ function FGsetFrequency(f)
   end
 end
 
+% change signal amplitude
 function FGsetAmplitude(Vpp)
   global visaFG;
   if visaFG>0
@@ -241,6 +294,21 @@ function FGsetAmplitude(Vpp)
   end
 end
 
+% set up measurement values displayed on scope (just for user)
+function MSOsetMeasurementDisplay(channelIn, channelOut)
+  global visaMSO;
+  if visaMSO>0
+    viWrite(visaMSO, ":MEASure:CLEar\n");
+%    viWrite(visaMSO, ":MEASure:VPP CHAN1\n");
+%    viWrite(visaMSO, ":MEASure:VPP CHAN2\n");
+    viWrite(visaMSO, ":MEASure:VRMS CYCLe,AC,CHAN1\n");
+    viWrite(visaMSO, ":MEASure:VRMS CYCLe,AC,CHAN2\n");
+    viWrite(visaMSO, ":MEASure:PHASe CHAN2,CHAN1\n");
+    viWrite(visaMSO, ":MEASure:SHOW ON\n");
+  end
+end
+
+% set time range of trace
 function MSOsetTimeRange(seconds)
   global visaMSO;
   if visaMSO>0
@@ -250,6 +318,7 @@ function MSOsetTimeRange(seconds)
   end
 end
 
+% set amplitude range (full scale)
 function MSOsetAmpRange(channel, Vpp)
   global visaMSO;
   if visaMSO>0
@@ -259,17 +328,20 @@ function MSOsetAmpRange(channel, Vpp)
   end
 end
 
-function result = measure(f, Vpp, channelIn, channelOut)
+% get results from scope, assuming valid time and y settings
+function result = measure(channelIn, channelOut, f, Vpp)
+  % NOTE f and Vpp used for simulation only
   global visaMSO;
-  if visaMSO==0
-    vIn = Vpp;
+  if visaMSO==0 % simulate data of low pass
+    vIn = Vpp/sqrt(2);
     G = 1./(1+1i*f/1000);
     vOut = abs(Vpp*G);
     gain = abs(G);
     phase = rad2deg(arg(G));
-  else
-    vIn = str2num(viQuery(visaMSO, [":MEAS:VPP? CHAN" num2str(channelIn) "\n"], 100));
-    vOut = str2num(viQuery(visaMSO, [":MEAS:VPP? CHAN" num2str(channelOut) "\n"], 100));
+  else % get actual data from scope
+    % NOTE measuring RMS is more accurate than VPP in case of noise on any channel
+    vIn = str2num(viQuery(visaMSO, [":MEAS:VRMS? CHAN" num2str(channelIn) "\n"], 100));
+    vOut = str2num(viQuery(visaMSO, [":MEAS:VRMS? CHAN" num2str(channelOut) "\n"], 100));
     gain = abs(vOut/vIn);
     phase = str2num(viQuery(visaMSO, [":MEAS:PHASe? CHAN" num2str(channelOut) ",CHAN" num2str(channelIn) "\n"], 100));
     if phase>400
@@ -280,79 +352,80 @@ function result = measure(f, Vpp, channelIn, channelOut)
   result = [f vIn vOut gain gainDB phase];
 end
 
+% run a measurement by scanning all frequencies
 function runPressed(source, event)
   global configTable;
-  global visaMSO;
-  config = cell2mat(get(configTable, "data")(:,2)); % vector of numbers
-  fStart = config(1);
-  fStop = config(2);
-  nPoints = config(3);
-  logF = config(4);
-  Vpp = config(5);
-  channelIn = config(6);
-  channelOut = config(7);
-  if logF==1
-    f = logspace(log10(fStart), log10(fStop), nPoints);
-  else
-    f = linspace(log10(fStart), log10(fStop), nPoints);
-  end
-  FGsetAmplitude(Vpp);
   global dataTable;
-  data = NA(nPoints, 6);
-  data(:,1) = f;
+  global visaMSO;
+  % blank data table
+  data = defaultData(); % blank table with frequencies to measure
   set(dataTable, "data", data);
+  config = get(configTable, "data"); % vector of numbers in column 2
+  % set up output level
+  Vpp = cell2mat(config(6,2));
+  FGsetAmplitude(Vpp);
+  % get channels to use
+  channelIn = cell2mat(config(9,2));
+  channelOut = cell2mat(config(10,2));
+  MSOsetMeasurementDisplay(channelIn, channelOut); % measurements on scope
+  % actual settings for both channels
   inRange = str2num(viQuery(visaMSO, [":CHAN" num2str(channelIn) ":RANGE?"], 100));
   outRange = str2num(viQuery(visaMSO, [":CHAN" num2str(channelOut) ":RANGE?"], 100));
+  f = data(:,1);
   for n = 1:length(f)
     FGsetFrequency(f(n));
-    MSOsetTimeRange(2/f(n)); % set time range
-    pause(0.2);
+    MSOsetTimeRange(2/f(n)); % set time range to 2 periods
     % adjust range on input channel
-    while (inRange>0.160) && (inRange<100)
+    stopAdjust = 0;
+    for adjustLoop=1:10 % time out if not converging
       vppIn = str2num(viQuery(visaMSO, [":MEAS:VPP? CHAN" num2str(channelIn) "\n"], 100));
       if vppIn > 0.95*inRange
         inRange = inRange*2;
+        if inRange>50
+          inRange=50; stopAdjust=1;
+        end
         MSOsetAmpRange(channelIn, inRange);
       elseif vppIn < 0.4*inRange
         inRange = inRange/2; % 1.2*vppIn;
+        if inRange<0.08
+          inRange=0.08; stopAdjust=1;
+        end
         MSOsetAmpRange(channelIn, inRange);
       else
         break;
       end
     end
     % adjust range on output channel
-    while (outRange>0.160) && (outRange<100)
+    stopAdjust = 0;
+    for adjustLoop=1:10 % time out if not converging
       vppOut = str2num(viQuery(visaMSO, [":MEAS:VPP? CHAN" num2str(channelOut) "\n"], 100));
       if vppOut > 0.95*outRange
         outRange = outRange*2;
+        if outRange>50
+          outRange = 50; stopAdjust=1;
+        end
         MSOsetAmpRange(channelOut, outRange);
       elseif vppOut < 0.4*outRange
         outRange = outRange/2; % 1.2*vppIn;
+        if outRange < 0.08
+          outRange = 0.08; stopAdjust=1;
+        end
         MSOsetAmpRange(channelOut, outRange);
       else
+        break; % in expected range
+      end
+      if stopAdjust==1
         break;
       end
     end
-    data(n,:) = measure(f(n), Vpp, channelIn, channelOut);
-    set(dataTable, "data", data);
+    data(n,:) = measure(channelIn, channelOut, f(n), Vpp); % get measurements
+    set(dataTable, "data", data); % show in table
   end
-  figure(2);
-  hGain = subplot(2,1,1);
-  plot(data(:,1), data(:,5)); grid on;
-  xlabel("f [Hz]");
-  ylabel("gain [dB]");
-  hPhase = subplot(2,1,2);
-  plot(data(:,1), data(:,6)); grid on;
-  xlabel("f [Hz]");
-  ylabel("phase [deg]");
-  if logF==1
-    set(hGain, "XScale", "log");
-    set(hPhase, "XScale", "log");
-  end
-  linkaxes([hGain, hPhase], "x");
+  updatePlot([], []); % trigger plot update
 end
-% runButton =
+
 uicontrol(uiFig,
+  "style", "pushbutton",
   "units", "normalized",
   "position", [0.0 0.0 0.26 0.1],
   "string", "RUN",
